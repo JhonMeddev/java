2e3ab623539125a42aff3468f1348c94f3739392
Jhonatan Medeiros <jhonatan1medeiros@gmail.com>
2022-09-22 09:52:09 -0300

    Padrões e modificador static

---
+++ Aula-2/src/ex3/Principal.java
@@ -0,0 +1,14 @@
+package ex3;
+
+public class Principal {
+	
+	public static void main(String[] args) {		
+		
+		Carro c = new Carro();
+		
+		
+		
+		System.out.println(c.milhasParaMetros(90));
+	}
+
+}
