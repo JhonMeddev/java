d83e43afcbd6c086b99d0ad5dca9bea5725fe983
Jhonatan Medeiros <jhonatan1medeiros@gmail.com>
2022-10-11 12:29:36 -0300

    Encapsulamento

---
+++ Aula-4/Encapsulamento/src/empresa/Main.java
@@ -0,0 +1,17 @@
+package empresa;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Horario aula = new Horario();
+		
+		aula.setHora(24);
+		//aula.minuto=30;
+		//aula.segundo=0;
+		
+
+
+	}
+
+}
