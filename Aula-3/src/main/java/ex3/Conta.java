740a9c64db4a3be80e302db8eebd313474acd7c1
Jhonatan Medeiros <jhonatan1medeiros@gmail.com>
2022-10-10 23:04:03 -0300

    ex3

--- Aula-3/src/main/java/ex3/Conta.java
+++ Aula-3/src/main/java/ex3/Conta.java
@@ -1,5 +1,48 @@
 package ex3;
 
+/*
+ * -Crie uma classe conta para administrar contas correntes
+ * de um banco.
+ * */
+
 public class Conta {
+	
+	public int numero;
+	public String titular;
+	public float saldo;
+	public float limite;
+	
+	public Conta(int numero, String titular, float saldo, float limite) {
+		super();
+		this.numero = numero;
+		this.titular = titular;
+		this.saldo = saldo;
+		this.limite = limite;
+	}
+	
+	boolean sacar(float valor) {
+		if (valor > limite || valor > saldo) {
+			return false;
+		}
+		saldo -= valor;
+		return true;
+	}
+	
+	boolean depositar(float valor) {
+		if(valor <= 0) {
+			return false;
+		}
+		saldo+= valor;
+		return true;
+	}
+	
+
+	
+	void info() {
+		System.out.println("Nome: "+ titular);
+		System.out.println("Numero: "+ numero);
+		System.out.println("Saldo: "+ saldo);
+		System.out.println("Limite: "+ limite );
+	}
 
 }
---
+++ Aula-3/src/main/java/ex3/Main.java
@@ -0,0 +1,19 @@
+package ex3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Conta c1 = new Conta(111, "Mario", 2000, 500);
+		
+		c1.info();
+		
+		c1.sacar(200);
+		
+		c1.depositar(500);
+		
+		c1.info();
+
+	}
+
+}
