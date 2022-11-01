f4069d536653da00d6f6fa1510446a6d77f606ab
Jhonatan Medeiros <jhonatan1medeiros@gmail.com>
2022-10-31 23:29:59 -0300

    Exercicio 2

---
+++ Aula-7/src/Exercicios/ex2/Main.java
@@ -0,0 +1,25 @@
+package Exercicios.ex2;
+
+public class Main {
+	
+	public static int[] instanciaArray(int n) {
+		
+		return new int[n];
+	}
+
+	public static void main(String[] args) {
+		
+		int tam = -10;
+		int arr[];
+		
+		try {
+			arr = instanciaArray(tam);
+			for(int i=0; i<tam;i++) {
+				System.out.println(arr[i]);
+			}
+		} catch (NegativeArraySizeException e) {
+			System.out.println("Valor invalido, digite outro");
+			System.out.println(e.getMessage());
+		}
+	}
+}
